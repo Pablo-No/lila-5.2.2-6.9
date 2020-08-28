d49780804b3fb507c37c922cd016707db9450867
Pablo-No <65043486+Pablo-No@users.noreply.github.com>
2020-08-28 18:21:55 +0200

    Create chessPosition object constructor

---
+++ implementation/chess.js
@@ -0,0 +1,20 @@
+function chessPosition(array possitionArray) {
+  this.positionArray = possitionArray;
+  this.fen.start = "";
+  for(y of this.positionArray){
+    let c = 0;
+    for(x of y){
+      if(y==""){
+        c++;
+      }
+      else{
+        if(c>0){
+          this.fen.start.concat(c.toString()+y);
+        }
+        else {
+          this.fen.start.concat(y);
+        }
+      }
+    }
+  }
+}
